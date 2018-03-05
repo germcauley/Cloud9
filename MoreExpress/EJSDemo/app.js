830ca089b2d0f9eb782e38227d72304515010276
germcauley <geraldmcauley@gmail.com>
2018-03-05 22:54:08 +0000

    Using JS in EJS

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -15,6 +15,16 @@ app.get("/fallinlovewith/:thing", function(req, res){
 //fallinlovewith/rusty
 //fallinlovewith/pomsky
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My pet bunny", author: "Tom"},
+        {title: "Can you believe this!", author: "Bob"},
+        {title: "Another great post", author: "Mary"}
+        
+        ]
+        res.render("posts.ejs", {posts: posts})
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening");
