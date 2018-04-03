1e588a16511375142c991ffd341875ab78541ea0
germcauley <geraldmcauley@gmail.com>
2018-04-03 22:11:28 +0000

    Learned new REST routes, added description route to camps

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -11,14 +11,17 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
+    
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //         {
 //             name: "Smokey Bear Forest",
-//             image: "http://www.photosforclass.com/download/pixabay-548022?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fec31b90f2af61c22d2524518b7444795ea76e5d004b0144397f0c678a1ebb7_960.jpg&user=bhossfeld"
+//             image: "http://www.photosforclass.com/download/pixabay-548022?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fec31b90f2af61c22d2524518b7444795ea76e5d004b0144397f0c678a1ebb7_960.jpg&user=bhossfeld",
+//             description: "This is a huge forest camp. No bathrooms or running water. A real survivors wilderness!"
             
 //         },
 //         function(err, campground){
@@ -37,6 +40,7 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+//INDEX - show all campgrounds
 //display campgrounds
 app.get("/campgrounds", function(req,res){
     //Get all campgrounds from DB
@@ -44,18 +48,20 @@ app.get("/campgrounds", function(req,res){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds",{campgrounds:allCampgrounds});
+            res.render("index",{campgrounds:allCampgrounds});
         }
     });
 });
 
+//CREATE
 //post new camp ground and redirect
 app.post("/campgrounds", function(req, res){
    
    //get data from form and add to campgrounds array
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground = ("campgrounds",{name:name, image:image});
+   var desc = req.body.description;
+   var newCampground = ("campgrounds",{name:name, image:image, description: desc});
    //CREATE NEW CAMPGROUND AND SAVE TO DB
    Campground.create(newCampground,function(err, newlyCreated){
        if(err){
@@ -71,6 +77,22 @@ app.post("/campgrounds", function(req, res){
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
 });
+ 
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+})
+ 
+ 
  
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp Server Has Started!");
