b1205bde1efa16947ba59d4dc3fbc9955fe281aa
germcauley <geraldmcauley@gmail.com>
2018-04-07 13:08:43 +0000

    post route added

--- RESTFULRouting/blog_app/app.js
+++ RESTFULRouting/blog_app/app.js
@@ -30,7 +30,7 @@ app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
-
+//INDEX ROUTE
 app.get("/blogs",function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -40,6 +40,23 @@ app.get("/blogs",function(req, res){
         }
     });
 });
+
+//NEW ROUTE
+app.get("/blogs/new", function(req,res){
+    res.render("new");
+});
+//CREATE ROUTE
+app.post("/blogs", function(req,res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }else{
+            //then redirect to index
+            res.redirect("/blogs");
+        }
+    });
+})
 
 
 
