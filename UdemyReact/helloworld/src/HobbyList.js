af966bc3af8e97e125f91b3713603eb9773bd5bc
germcauley <geraldmcauley@gmail.com>
2019-07-24 21:13:09 +0000

    add option to list

--- UdemyReact/helloworld/src/HobbyList.js
+++ UdemyReact/helloworld/src/HobbyList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class HobbyList extends Component {
     render() {
-        const hobbies = ["Sleeping", "Eating", "Cuddling"];
+        const hobbies = ["Sleeping", "Eating", "Cuddling","Purring"];
         const style = { fontSize: "1.5em" };
         return (
             <ul>
