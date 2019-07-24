1054182892e3075bc582ba9ed0e01e0bbbf10672
EC2 Default User <ec2-user@ip-172-31-22-140.eu-west-1.compute.internal>
2019-07-24 21:04:34 +0000

    trying to merge to master

--- UdemyReact/helloworld/src/App.js
+++ UdemyReact/helloworld/src/App.js
@@ -14,6 +14,7 @@ class App extends Component{
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>a p element</p>
+          <p>test</p>
           <a
             className="App-link"
             href="https://reactjs.org"
