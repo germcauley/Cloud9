f62d4806b2c2d47fc673e8666019ab5fb4b4e162
germcauley <geraldmcauley@gmail.com>
2019-08-27 22:12:02 +0000

    Created RecipeList componet,need to style

--- UdemyReact/recipe-props-starter/src/RecipeList.js
+++ UdemyReact/recipe-props-starter/src/RecipeList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Recipe from './Recipe';
-import 'RecipeList.css';
+import PropTypes from 'prop-types';
+import './RecipeList.css';
 
 class RecipeList extends Component {
     static defaultProps = {
@@ -10,13 +11,31 @@ class RecipeList extends Component {
                 instructions:"Open jar of Spaghetti sauce",
                 ingredients:["pasta", "8 cups water", "1 box sugar"],
                 img: "spaghetti.jpg"
+            },
+            {
+                title: "Milkshake",
+                instructions:"Add milk and shake!",
+                ingredients:["pasta", "8 cups water", "1 box sugar"],
+                img: "spaghetti.jpg"
             }
             
             
             
             ]
     }
+    
+    static propTypes = {
+        recipes: PropTypes.arrayOf(PropTypes.object)
+    }
     render(){
+        const recipes = this.props.recipes.map((r, index) =>(
+            <Recipe key ={index} {...r}/>
+            ));
+      return (
+          <div className ="recipe-list">
+            {recipes}
+          </div>
+          )      
        
     }
 }
